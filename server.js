89fe9fb85202449649478137e9d445ab330215b3
AishaKhalfan <aishakhalfan45@gmail.com>
2023-09-05 12:48:41 +0300

    FIRST API

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 5000;
-const routes =  require('./routes/index');
+import express from 'express';
+import router from './routes/index';
 
+const app = express();
+app.use(express.json());
+app.use(router);
+
+console.log(process.env.DB_HOST);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
+
+export default app;
